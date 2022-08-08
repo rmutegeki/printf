ffbd566c0ea8c94607edb588cfc8208c491bf9fa
Mutegeki Ronald <murogive@gmail.com>
2022-08-08 06:02:48 -0700

    Added new headers to main.h

--- main.h
+++ main.h
@@ -11,18 +11,17 @@
 /*macros*/
 /*structs*/
 /**
- * struct directive_struct - printf directives and helper print functions
- * @ch: the directive
- * @func: pointer to the helper function
+ * struct specifier_struct - printf specifiers and helper print functions
+ * @ch: the specifier
+ * @func: pointer to the specifier function
  */
-typedef struct directive_struct
+typedef struct specifier_struct
 {
 	char ch;
 	int (*func)(char *buffer, va_list arg_list);
-} directive_t;
+} specifiers_t;
 
 
-int _putchar(char c);
 /*Our custom printf*/
 int _printf(const char *format, ...);
 
@@ -33,13 +32,15 @@ int skip_spaces(const char *format);
 int _strlen(char *s);
 int _putchar(char c);
 int add_to_buffer(char *buffer, char c);
-int (*get_directive(char c))(char *buffer, va_list arg_list);
+int (*match_specifier(char c))(char *buffer, va_list arg_list);
 void print_buffer(char *);
 /*end helper function prototypes*/
 
 /*functions that write to buffer*/
 int p_char(char *buffer, va_list arg_list);
 int p_string(char *buffer, va_list arg_list);
+int p_string_hex(char *buffer, va_list arg_list);
+int p_pointer(char *buffer, va_list arg_list);
 int p_percent(char *buffer, va_list arg_list);
 int p_int(char *buffer, va_list arg_list);
 int p_uint(char *buffer, va_list arg_list);
